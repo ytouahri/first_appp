e8a73dc38909b4d96e102777012341b8af36d610
Yassine Touahri <ytouahri@uncc.edu>
2016-10-14 03:58:09 +0000

    Update Gemfile.lock for Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+group :development, :test do
+  gem 'sqlite3',     '1.3.9'
+  gem 'byebug',      '3.4.0'
+  gem 'web-console', '2.0.0.beta3'
+  gem 'spring',      '1.1.3'
+end
